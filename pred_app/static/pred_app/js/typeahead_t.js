27d63853d6933d9c136e0dd496adfe3d050b1434
Tejas pandav <99353991+tejas248@users.noreply.github.com>
2024-04-04 14:28:30 +0530

    Update typeahead_t.js

--- pred_app/static/pred_app/js/typeahead_t.js
+++ pred_app/static/pred_app/js/typeahead_t.js
@@ -1,8 +1,4 @@
-/*!
- * typeahead.js 0.10.2
- * https://github.com/twitter/typeahead.js
- * Copyright 2013-2014 Twitter, Inc. and other contributors; Licensed MIT
- */
+
 
 (function($) {
     var _ = {
